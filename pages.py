e0365ee232e3491d764efa45064a1aa447acd4f6
madscientist-314 <tomkirby314@gmail.com>
2023-09-22 18:26:16 +0000

    Initial Commit 22/09/23
    init

--- pages.py
+++ pages.py
@@ -1,67 +1,63 @@
 from threading import Thread
 from flask import Flask, send_file
 
-app = Flask(
-  __name__,
-  template_folder = 'pages',
-  static_folder = 'static'
-)
+app = Flask(__name__, template_folder='pages', static_folder='static')
+
 
 def run():
-    app.run(host='0.0.0.0', port=8080)
+  app.run(host='0.0.0.0', port=8080)
+
 
 def runwebsite():
-    t = Thread(target=run)
-    t.start()
+  t = Thread(target=run)
+  t.start()
+
 
 @app.route('/')
-def home():
-    return send_file('pages/index.html')
+@app.route('/home')
+def index():
+  return send_file('pages/index.html')
+
 
 @app.errorhandler(404)
 def page_not_found(e):
-    return send_file('pages/error.html')
+  return send_file('pages/error400.html')
 
-@app.route('/bronze')
-def bronze():
-    return send_file('bronze.html')
 
-@app.route('/bronze/expedition')
-def bronze_exped():
-    return send_file('bexpedition.html')
+@app.errorhandler(500)
+def internal_server_error(e):
+  return send_file('pages/error500.html')
 
-@app.route('/bronze/skill')
-def bronze_skill():
-    return send_file('BSkill.html')
 
-@app.route('/bronze/volunteering')
-def bronze_volunteer():
-    return send_file('bvolunteering.html')
+@app.route('/news')
+def news():
+  return send_file('pages/news.html')
 
-@app.route('/bronze/physical')
-def bronze_physical():
-    return send_file('bphysical.html')
 
-@app.route('/silver')
+@app.route('/hire')
+def hire():
+  return send_file('pages/hire.html')
+
+
+@app.route('/faqs')
+def faqs():
+  return send_file('pages/faqs.html')
+
+
+@app.route('/contact')
+def contact():
+  return send_file('pages/contact.html')
+
+
+@app.route('/login')
+def login():
+  return send_file('pages/login.html')
+
+
+@app.route('/login/signup')
 def silver():
-    return send_file('silver.html')
+  return send_file('pages/signup.html')
 
-@app.route('/silver/expedition')
-def silver_exped():
-    return send_file('sexpedition.html')
-
-@app.route('/silver/skill')
-def silver_skill():
-    return send_file('sskill.html')
-
-@app.route('/silver/volunteering')
-def silver_volunteer():
-    return send_file('svolunteering.html')
-
-@app.route('/silver/physical')
-def silver_physical():
-    return send_file('sphysical.html')
-
-@app.route('/gold')
-def gold():
-  return send_file('gold.html')
+@app.route('/login/8iNk6yncgPwtRs5bgRA')
+def admin():
+  return send_file('pages/hidden/admin.html')
